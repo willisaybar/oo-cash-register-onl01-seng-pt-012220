7b41f967d88ff68936c7858e27ff86c0bafd905e
Willis Aybar <willisaybar@gmail.com>
2020-03-10 01:35:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
 
+  attr_accessor = :total
+
+
+  def initialize(total = 0)
+    @total = total
+  end
+
+  def discount(discount = 20)
+    @discount = discount
+  end
+
+  def cash_register_total
+    self.total += 100
+  end
+
+end
